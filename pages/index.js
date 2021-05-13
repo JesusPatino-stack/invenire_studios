0450e23e29894b9ecb2280126ebf4babce3741e8
JesusPatino <eljesus788@gmail.com>
2021-05-13 16:22:44 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello Neil</a>
+          Hello Neil
         </h1>
 
         <p className={styles.description}>
