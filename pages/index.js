5193a6b9eef5a1c0079288aa541f314d953a53b4
JesusPatino <eljesus788@gmail.com>
2021-05-13 17:11:57 -0400

    updating layout

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default function Home() {
   return (
@@ -12,17 +14,65 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        
         <h1 className={styles.title}>
           Invenire Studios
         </h1>
+        <Image src='/img/hero.bmp' alt='hero' width={600} height={500}/>
         <div>Artist and Technologist</div>
 
       </main>
 
-      <footer className={styles.footer}>
-        <span>Neal Butler</span>
-        <span>706.463.8523</span>
-      </footer>
+      <Footer/>
     </div>
   )
 }
+
+function Footer() {
+  const classes = useStyles();
+  return (
+    <footer className={classes.footer}>
+        <Typography variant="h5" align="center" gutterBottom>
+          Contact
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+        Neal Butler
+        </Typography>
+        <Typography variant="subtitle2" align="center" color="textSecondary" component="p">
+        706.463.8523
+        </Typography>
+      </footer>
+  )
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
