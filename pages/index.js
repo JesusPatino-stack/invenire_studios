20b14d4fa5a3618eb478243af5f4a8173bcf975d
JesusPatino <eljesus788@gmail.com>
2021-05-13 20:07:59 -0400

    css updates

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 // import styles from "../styles/Home.module.css";
@@ -5,26 +6,98 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import PhoneIcon from "@material-ui/icons/Phone";
 import { Box } from "@material-ui/core";
-import '@fontsource/roboto';
+import "@fontsource/roboto";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Slide from "@material-ui/core/Slide";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 
 // import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 // let theme = createMuiTheme();
 // theme = responsiveFontSizes(theme);
 
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+function HideAppBar(props) {
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar style={{ background: "black", display:'flex'}}>
+          <Toolbar>
+            <Typography variant="h6" style={{ background: "black", marginLeft: "auto", }}></Typography>
+            <SimpleBottomNavigation
+              style={{ display: "flex", background: "black", marginRight: "auto", }}
+            />
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+
+      <hr />
+    </>
+  );
+}
+
+export function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+      style={{ background: "black" }}
+    >
+      <BottomNavigationAction label="Art" style={{ color: "white" }} />
+      <BottomNavigationAction label="CAD" style={{ color: "white" }} />
+      <BottomNavigationAction label="Events/Promotions" style={{ color: "white" }} />
+    </BottomNavigation>
+  );
+}
 export default function Home() {
   const classes = useStyles();
   return (
     // <ThemeProvider theme={theme}>
     <div className={classes.container}>
       <Head>
-        <title >Invenire Studios</title>
+        <title>Invenire Studios</title>
         <meta name="description" content="Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={classes.main} >
+      <HideAppBar />
+      <main className={classes.main}>
         {/* <div className={classes.heroContent} >  */}
-        <img  className={classes.heroContent}src="/img/hero.bmp" />
+        <img className={classes.heroContent} src="/img/hero.bmp" />
         {/* </div> */}
         {/* <Image src="/img/hero.bmp" alt="hero" layout="fill" /></div> */}
         {/* <Box m={5}></Box> */}
@@ -32,18 +105,11 @@ export default function Home() {
           Invenire Studios
         </Typography> */}
         {/* <Image src="/img/hero.bmp" alt="hero" width={600} height={500} /> */}
-        <Typography
-          variant="h4"
-          align="center"
-          color="white"
-          component="p"
-        >
+        <Typography variant="h4" align="center" color="white" component="p">
           Artist and Technologist
         </Typography>
         <Footer />
       </main>
-
-      
     </div>
     // </ThemeProvider>
   );
@@ -56,21 +122,14 @@ function Footer() {
       {/* <Typography variant="h5" align="center" gutterBottom>
         Contact
       </Typography> */}
-      <Typography
-        variant="h3"
-        align="center"
-        color="white"
-        component="p"
-      >
+      <Typography variant="h5" align="center" color="white" component="p" style={{margin: "auto", padding:'10px'}}>
         Neal Butler
       </Typography>
-      <Typography
-        variant="h4"
-        align="center"
-        color="white"
-        component="p"
-      >
+      <Typography variant="h5" align="center" color="white" component="p" style={{margin: "auto", padding:'10px'}}>
         <a href="tel:7064638523">706.463.8523</a>
+      </Typography>
+      <Typography variant="h5" align="center" color="white" component="p" style={{margin: "auto", padding:'10px'}}>
+        <a href="">InvenireStudios@gmail.com</a>
       </Typography>
     </footer>
   );
@@ -85,7 +144,7 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     alignItems: "center",
     height: "100%",
-    backgroundColor: 'black'
+    backgroundColor: "gray",
   },
   main: {
     padding: "5rem 0",
@@ -94,18 +153,19 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: 'black',
-    verticalAlign: 'top'
+    backgroundColor: "gray",
+    verticalAlign: "top",
+    maxWidth: "1080px",
   },
   icon: {
     marginRight: theme.spacing(2),
   },
   heroContent: {
-    backgroundColor: 'black',
+    backgroundColor: "gray",
     padding: theme.spacing(8, 0, 6),
     // height:"100rem",
-    width:"100%",
-    alignContent: 'center',
+    width: "100%",
+    // alignContent: 'center',
     // verticalAlign: 'top'
   },
   heroButtons: {
@@ -127,31 +187,34 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   footer: {
-    backgroundColor: 'black',
-    color: 'white',
+    backgroundColor: "gray",
+    color: "white",
     padding: theme.spacing(6),
-    width: '100%'
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignVertical: "center"
   },
   title: {
-    font: 'Roboto',
+    font: "Roboto",
     weight: "light",
     fontSize: "96px",
-    letterSpacing: "-1.5px"
+    letterSpacing: "-1.5px",
   },
   typography: {
     // Use the system font instead of the default Roboto font.
     fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
+      "-apple-system",
+      "BlinkMacSystemFont",
       '"Segoe UI"',
-      'Roboto',
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
+      "Arial",
+      "sans-serif",
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"',
-    ].join(','),
+    ].join(","),
     subtitle1: {
       fontSize: 12,
     },
@@ -159,14 +222,13 @@ const useStyles = makeStyles((theme) => ({
       fontWeight: 500,
     },
     button: {
-      fontStyle: 'italic',
+      fontStyle: "italic",
     },
     h3: {
-      color: 'white'
+      color: "white",
     },
     h4: {
-      color: 'white'
-    }
+      color: "white",
+    },
   },
 }));
-
