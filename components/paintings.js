b962585d35adcc59b60403e92e1a8e9744146eb5
JesusPatino <eljesus788@gmail.com>
2021-07-04 12:10:39 -0400

    fourth july

--- components/paintings.js
+++ components/paintings.js
@@ -8,12 +8,14 @@ export default function getPaintings() {
                 condition:"fourth",
                 get_tickets: "http://www.nightfallchattanooga.com/",
                 g_maps:'https://www.google.com/maps/place/Miller+Plaza+Pavilion/@35.0462632,-85.3110403,17z/data=!4m9!1m2!2m1!1smiller+plaza!3m5!1s0x88605e7ca27ad5d9:0x26ac3a1078187c2!8m2!3d35.0461191!4d-85.3091081!15sCgxtaWxsZXIgcGxhemGSARdub25fcHJvZml0X29yZ2FuaXphdGlvbg?hl=en',
+                price: ''
               },
               {
                 title:"Cats",
                 img:"/img/paintings/jpg/3.jpg",
                 description:"On display at the 4th of July event at Miller Plaza in Chattanooga, TN.",
                 condition:"fourth",
+                price: '',
                 get_tickets: "http://www.nightfallchattanooga.com/",
                 g_maps:'https://www.google.com/maps/place/Miller+Plaza+Pavilion/@35.0462632,-85.3110403,17z/data=!4m9!1m2!2m1!1smiller+plaza!3m5!1s0x88605e7ca27ad5d9:0x26ac3a1078187c2!8m2!3d35.0461191!4d-85.3091081!15sCgxtaWxsZXIgcGxhemGSARdub25fcHJvZml0X29yZ2FuaXphdGlvbg?hl=en',
               },
@@ -22,6 +24,7 @@ export default function getPaintings() {
                 img:"/img/paintings/jpg/4.jpg",
                 description:"On display at the 4th of July event at Miller Plaza in Chattanooga, TN.",
                 condition:"fourth",
+                price: '',
                 get_tickets: "http://www.nightfallchattanooga.com/",
                 g_maps:'https://www.google.com/maps/place/Miller+Plaza+Pavilion/@35.0462632,-85.3110403,17z/data=!4m9!1m2!2m1!1smiller+plaza!3m5!1s0x88605e7ca27ad5d9:0x26ac3a1078187c2!8m2!3d35.0461191!4d-85.3091081!15sCgxtaWxsZXIgcGxhemGSARdub25fcHJvZml0X29yZ2FuaXphdGlvbg?hl=en',
               },
@@ -30,6 +33,7 @@ export default function getPaintings() {
                 img:"/img/paintings/jpg/2.jpg",
                 description:"On display at the 4th of July event at Miller Plaza in Chattanooga, TN.",
                 condition:"fourth",
+                price: '',
                 g_maps:'https://www.google.com/maps/place/Level+Up+Arcade+and+Billiards/@34.999782,-85.2584417,17z/data=!4m13!1m7!3m6!1s0x88606772239c2303:0x4b1bc5421cb3a9be!2s3801+Ringgold+Rd,+Chattanooga,+TN+37412!3b1!8m2!3d34.999782!4d-85.256253!3m4!1s0x88606786be179a13:0xddbc1ca807d0bf19!8m2!3d35.0001654!4d-85.2569407',
               },
               {
@@ -37,6 +41,7 @@ export default function getPaintings() {
                 img:"/img/paintings/gif/bugs_behind_the_screen.gif",
                 description:`On display @ Level Up Arcade and Billiards in East Ridge, TN. \n Open 7 days a week from 5pm-3am`,
                 condition:"venue location",
+                price: '',
                 g_maps:'https://www.google.com/maps/place/Level+Up+Arcade+and+Billiards/@34.999782,-85.2584417,17z/data=!4m13!1m7!3m6!1s0x88606772239c2303:0x4b1bc5421cb3a9be!2s3801+Ringgold+Rd,+Chattanooga,+TN+37412!3b1!8m2!3d34.999782!4d-85.256253!3m4!1s0x88606786be179a13:0xddbc1ca807d0bf19!8m2!3d35.0001654!4d-85.2569407',
               },
             //   {
@@ -51,6 +56,7 @@ export default function getPaintings() {
                 img:"/img/paintings/gif/earth_and_sky.gif",
                 description:`On display @ Level Up Arcade and Billiards in East Ridge, TN. \n Open 7 days a week from 5pm-3am`,
                 condition:"venue location",
+                price: '',
                 g_maps:'https://www.google.com/maps/place/Level+Up+Arcade+and+Billiards/@34.999782,-85.2584417,17z/data=!4m13!1m7!3m6!1s0x88606772239c2303:0x4b1bc5421cb3a9be!2s3801+Ringgold+Rd,+Chattanooga,+TN+37412!3b1!8m2!3d34.999782!4d-85.256253!3m4!1s0x88606786be179a13:0xddbc1ca807d0bf19!8m2!3d35.0001654!4d-85.2569407',
               },
               {
@@ -58,6 +64,7 @@ export default function getPaintings() {
                 img:"/img/paintings/gif/electric_ice.gif",
                 description:"On display at the 4th of July event at Miller Plaza in Chattanooga, TN.",
                 condition:"fourth",
+                price: '',
                 g_maps:'https://www.google.com/maps/place/Level+Up+Arcade+and+Billiards/@34.999782,-85.2584417,17z/data=!4m13!1m7!3m6!1s0x88606772239c2303:0x4b1bc5421cb3a9be!2s3801+Ringgold+Rd,+Chattanooga,+TN+37412!3b1!8m2!3d34.999782!4d-85.256253!3m4!1s0x88606786be179a13:0xddbc1ca807d0bf19!8m2!3d35.0001654!4d-85.2569407',
               },
             //   {
@@ -72,6 +79,7 @@ export default function getPaintings() {
                 img:"/img/paintings/gif/mary_and_the_devil.gif",
                 description:`On display @ Level Up Arcade and Billiards in East Ridge, TN. \n Open 7 days a week from 5pm-3am`,
                 condition:"venue location",
+                price: '',
                 g_maps:'https://www.google.com/maps/place/Level+Up+Arcade+and+Billiards/@34.999782,-85.2584417,17z/data=!4m13!1m7!3m6!1s0x88606772239c2303:0x4b1bc5421cb3a9be!2s3801+Ringgold+Rd,+Chattanooga,+TN+37412!3b1!8m2!3d34.999782!4d-85.256253!3m4!1s0x88606786be179a13:0xddbc1ca807d0bf19!8m2!3d35.0001654!4d-85.2569407',
               },
               {
@@ -79,6 +87,7 @@ export default function getPaintings() {
                 img:"/img/paintings/gif/puzzle.gif",
                 description:`On display @ Level Up Arcade and Billiards in East Ridge, TN. \n Open 7 days a week from 5pm-3am`,
                 condition:"venue location",
+                price: '',
                 g_maps:'https://www.google.com/maps/place/Level+Up+Arcade+and+Billiards/@34.999782,-85.2584417,17z/data=!4m13!1m7!3m6!1s0x88606772239c2303:0x4b1bc5421cb3a9be!2s3801+Ringgold+Rd,+Chattanooga,+TN+37412!3b1!8m2!3d34.999782!4d-85.256253!3m4!1s0x88606786be179a13:0xddbc1ca807d0bf19!8m2!3d35.0001654!4d-85.2569407',
               },
               {
@@ -86,6 +95,7 @@ export default function getPaintings() {
                 img:"/img/paintings/jpg/idea_formation.jpg",
                 description:"On display at the 4th of July event at Miller Plaza in Chattanooga, TN.",
                 condition:"fourth",
+                price: '',
                 g_maps:'',
               },
               {
@@ -93,6 +103,7 @@ export default function getPaintings() {
                 img:"/img/paintings/gif/hurricane.gif",
                 description:`On display @ Level Up Arcade and Billiards in East Ridge, TN. \n Open 7 days a week from 5pm-3am`,
                 condition:"venue location",
+                price: '',
                 g_maps:'https://www.google.com/maps/place/Level+Up+Arcade+and+Billiards/@34.999782,-85.2584417,17z/data=!4m13!1m7!3m6!1s0x88606772239c2303:0x4b1bc5421cb3a9be!2s3801+Ringgold+Rd,+Chattanooga,+TN+37412!3b1!8m2!3d34.999782!4d-85.256253!3m4!1s0x88606786be179a13:0xddbc1ca807d0bf19!8m2!3d35.0001654!4d-85.2569407',
               },
           ]
